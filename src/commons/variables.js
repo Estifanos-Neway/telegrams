8efc36385eea25e6492dc1ebdd39d3689d21b948
Estifanos Neway <estifanos.neway.d@gmail.com>
2022-03-13 11:12:08 +0300

    edited session_data_dir and session_data_dir in common/variables

--- src/commons/variables.js
+++ src/commons/variables.js
@@ -1,4 +1,4 @@
-exports.session_data_dir = "data";
-exports.default_user_file = "default-user";
+exports.session_data_dir = "session-data";
+exports.default_user_file = "default";
 
 exports.login_first_text = "You are not logged in, use 'npm run login [name]' to login.";
